f494583af62cb7a53a3890b18a00c5e45b17251b
obiwan1715 <benatherton.news@gmail.com>
2016-10-13 14:28:42 +1000

    add new blinking light program with plug in LED

---
+++ blinking_light_from_book/blinking_light_from_book.ino
@@ -0,0 +1,13 @@
+// Blinking LED project from book
+
+int led = 13;
+void setup() {
+  pinMode(led, OUTPUT);
+}
+void loop() {
+  digitalWrite(led, HIGH);
+  delay(2000);
+  digitalWrite(led, LOW);
+  delay(2000);
+}
+
