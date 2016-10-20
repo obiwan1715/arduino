269c0d059b700026e873e56996c6fb6029070f86
obiwan1715 <benatherton.news@gmail.com>
2016-10-21 08:49:15 +1000

    added pushbutton-controlled LED from book

---
+++ pushbutton_led/pushbutton_led.ino
@@ -0,0 +1,21 @@
+/* project 1 pushbutton LED*/
+
+const int buttonPin = 2; // pushbutton connected to pin 2
+const int ledPin = 13; // LED connected to pin 13
+int buttonState = 0; // give pushbutton a value
+
+void setup() {
+  pinMode(ledPin, OUTPUT); // set LED pin as output
+  pinMode(buttonPin, INPUT); // set pushbutton pin as input
+
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin); // read input from pin 2
+  if (buttonState == HIGH) { //check if button is pressed
+    digitalWrite(ledPin, HIGH); //turn on LED
+  }
+  else {
+    digitalWrite(ledPin, LOW); //otherwise, turn off LED
+  }
+}
