1d0248acdf31c1daf2a926b32301b367b8e5556c
obiwan1715 <benatherton.news@gmail.com>
2016-10-22 15:08:30 +1000

    added pushbutton led sketch written without instructions

---
+++ pushbutton_led_solo/pushbutton_led_solo.ino
@@ -0,0 +1,31 @@
+// push button led written from scratch
+
+// set variables
+
+const int led = 13;
+const int button = 2;
+int buttonState = 0;
+
+
+//set input/output states
+
+void setup() {
+  pinMode (led, OUTPUT);
+  pinMode (button, INPUT);
+
+}
+
+// check for button to be pushed, and if it is, light up the LED
+
+void loop() {
+  buttonState = digitalRead(button);
+
+  if (buttonState == HIGH) {
+    digitalWrite (led, HIGH);
+  }
+
+  else {
+    digitalWrite (led, LOW);
+  }
+
+}
