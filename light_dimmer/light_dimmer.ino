3ef0e2cb01c9451998ea22b9bdc2e192960ec9f8
obiwan1715 <benatherton.news@gmail.com>
2016-11-26 12:30:07 +1000

    add light dimmer

---
+++ light_dimmer/light_dimmer.ino
@@ -0,0 +1,19 @@
+//light dimmer project from arduino book
+//uses potentiometer, single LED
+
+int potPin = A0;
+int potValue = 0;
+int led = 9;
+
+
+void setup() {
+  pinMode(led, OUTPUT);
+
+}
+
+void loop() {
+  potValue = analogRead(potPin);
+  analogWrite(led, potValue/4);
+  delay(10);
+
+}
