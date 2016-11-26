397645c64bf7c16447de291b816844618cd3f3f1
obiwan1715 <benatherton.news@gmail.com>
2016-11-26 17:25:53 +1000

    bar graph with 9 leds

---
+++ led_bar_graph/led_bar_graph.ino
@@ -0,0 +1,24 @@
+const int analogPin = A0; // pin connects to potentiometer
+const int ledCount = 9; //number of LEDs
+int ledPins[] = {2,3,4,5,6,7,8,9,10}; //pins connected to LEDs
+
+
+void setup() {
+  for (int thisLed = 0; thisLed < ledCount; thisLed++) {
+    pinMode(ledPins[thisLed], OUTPUT); // set the LED pins as output
+    }
+   }
+
+void loop() {
+  int sensorReading = analogRead(analogPin); //analog input from potentiometer
+  int ledLevel = map(sensorReading, 0, 1023, 0, ledCount);
+  for (int thisLed = 0; thisLed < ledCount; thisLed++) {
+    if (thisLed<ledLevel) { //turn on LEDs in sequence
+      digitalWrite(ledPins[thisLed], HIGH);
+    }
+    else { //turn off LEDs in sequence
+      digitalWrite(ledPins[thisLed], LOW);
+    }
+  }
+
+}
---
+++ led_bar_graph/three_led_bar_graph/three_led_bar_graph.ino
@@ -0,0 +1,29 @@
+//this version of led bar graph only uses three leds
+
+//this one works fine
+
+
+const int analogPin = A0; // pin connects to potentiometer
+const int ledCount = 9; //number of LEDs
+int ledPins[] = {2,3,4,5,6,8,9,10,11}; //pins connected to LEDs
+
+
+void setup() {
+  for (int thisLed = 0; thisLed < ledCount; thisLed++) {
+    pinMode(ledPins[thisLed], OUTPUT); // set the LED pins as output
+    }
+   }
+ 
+void loop() {
+  int sensorReading = analogRead(analogPin); //analog input from potentiometer
+  int ledLevel = map(sensorReading, 0, 1023, 0, ledCount); //takes sensor reading in a analog range from 0 to 1023, and maps it into a new range between 0 and ledCount
+  for (int thisLed = 0; thisLed < ledCount; thisLed++) {
+    if (thisLed < ledLevel) { //turn on LEDs in sequence
+      digitalWrite(ledPins[thisLed], HIGH);
+    }
+    else { //turn off LEDs in sequence
+      digitalWrite(ledPins[thisLed], LOW);
+    }
+  }
+
+}
