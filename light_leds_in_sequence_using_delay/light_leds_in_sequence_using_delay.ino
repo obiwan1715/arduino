19d2f6e923bbc4e13c3fb93d3682f99d066b73e9
obiwan1715 <benatherton.news@gmail.com>
2016-11-27 18:07:00 +1000

    made delay a variable

--- light_leds_in_sequence_using_delay/light_leds_in_sequence_using_delay.ino
+++ light_leds_in_sequence_using_delay/light_leds_in_sequence_using_delay.ino
@@ -3,6 +3,7 @@
 
 const int ledCount = 9; //number of LEDs
 int ledPins[] = {2,3,4,5,6,8,9,10,11}; //pins connected to LEDs
+int wait = 100; // amount of time to wait between each LED
 
 
 void setup() {
@@ -14,12 +15,12 @@ void setup() {
 void loop() {
   for (int thisLed = 0; thisLed < ledCount; thisLed++) {
     digitalWrite(ledPins[thisLed], HIGH);
-    delay(1000);
+    delay(wait);
   }
 
   for (int thisLed = (ledCount-1); thisLed >= 0; thisLed--) {
     digitalWrite(ledPins[thisLed], LOW);
-    delay(1000);
+    delay(wait);
   }
 }
 
