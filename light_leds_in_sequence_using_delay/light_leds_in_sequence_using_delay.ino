938ea51c2336cc1c58b5fe3bd54a502b75ec1cef
obiwan1715 <benatherton.news@gmail.com>
2016-11-27 16:57:36 +1000

    light leds using delay

---
+++ light_leds_in_sequence_using_delay/light_leds_in_sequence_using_delay.ino
@@ -0,0 +1,25 @@
+//modified LED bar graph. lights each LED in sequence and then powers them down in reverse order
+
+
+const int ledCount = 9; //number of LEDs
+int ledPins[] = {2,3,4,5,6,8,9,10,11}; //pins connected to LEDs
+
+
+void setup() {
+  for (int thisLed = 0; thisLed < ledCount; thisLed++) {
+    pinMode(ledPins[thisLed], OUTPUT); // set the LED pins as output
+    }
+   }
+ 
+void loop() {
+  for (int thisLed = 0; thisLed < ledCount; thisLed++) {
+    digitalWrite(ledPins[thisLed], HIGH);
+    delay(1000);
+  }
+
+  for (int thisLed = (ledCount-1); thisLed >= 0; thisLed--) {
+    digitalWrite(ledPins[thisLed], LOW);
+    delay(1000);
+  }
+}
+
