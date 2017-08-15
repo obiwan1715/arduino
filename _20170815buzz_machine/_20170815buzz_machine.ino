725cba2ab2a15f6d8a6f4cdff6e787b4ce0d547a
obiwan1715 <benatherton.news@gmail.com>
2017-08-15 19:45:06 +1000

    potentiometer controlled buzzer

---
+++ _20170815buzz_machine/_20170815buzz_machine.ino
@@ -0,0 +1,16 @@
+// turn a potentiometer to change the tone of the sound coming from a piezo buzzer
+
+int currentIn = A0; // set A0 as pin for analog input from potentiometer
+int soundOut = 9; // sets pin 9 for output to buzzer
+
+void setup() {
+pinMode (currentIn,INPUT); //set this pin as input
+pinMode (soundOut,OUTPUT); //set this pin as output
+}
+
+void loop() {
+  int noise = analogRead(currentIn); // take reading from potentiometer
+  int pitch = map(noise, 0, 1023, 0, 400); //change reading to a value between 0 (silent) and 400
+  tone(soundOut,pitch,20); //send tone to pin 9, play pitch, duration 20
+  delay(10); //pause 10ms then start again
+}
