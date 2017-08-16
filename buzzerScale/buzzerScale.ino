f0175d57f62d6e9dea4feda5f85e35f10f65c1d3
obiwan1715 <benatherton.news@gmail.com>
2017-08-17 09:43:52 +1000

    added silence between notes

--- buzzerScale/buzzerScale.ino
+++ buzzerScale/buzzerScale.ino
@@ -2,9 +2,8 @@
  I could't get this to work using this syntax
  tone(buzzer, notes[thisNote], wait);
  maybe its as simple as putting (wait) in brackets
- or maybe I never commented out the int wait
+ or maybe I commented out the int wait
  find a way to use for to set up the notes array at the beginning
- add lines to turn each tone off and get silence before the next one
  */
  
 int buzzer = 8;
@@ -22,9 +21,13 @@ void loop() {
   for (int thisNote=0; thisNote < noteCount; thisNote++) {
     tone(buzzer, notes[thisNote]);
     delay(1000);
+    noTone(buzzer);
+    delay(1000);
   }
  for (int thisNote=(noteCount-1); thisNote >0; thisNote--) {
     tone(buzzer, notes[thisNote]);
+    delay(1000);
+    noTone(buzzer);
     delay(1000);
   }
 }
