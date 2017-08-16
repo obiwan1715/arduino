54252293e21f86c87e0874aa465d695a27a08204
obiwan1715 <benatherton.news@gmail.com>
2017-08-16 16:32:43 +1000

    changed pitch on buzz machine

--- _20170815buzz_machine/_20170815buzz_machine.ino
+++ _20170815buzz_machine/_20170815buzz_machine.ino
@@ -10,7 +10,7 @@ pinMode (soundOut,OUTPUT); //set this pin as output
 
 void loop() {
   int noise = analogRead(currentIn); // take reading from potentiometer
-  int pitch = map(noise, 0, 1023, 0, 400); //change reading to a value between 0 (silent) and 400
+  int pitch = map(noise, 0, 1023, 0, 4000); //change reading to a value between 0 (silent) and 400
   tone(soundOut,pitch,20); //send tone to pin 9, play pitch, duration 20
   delay(10); //pause 10ms then start again
 }
