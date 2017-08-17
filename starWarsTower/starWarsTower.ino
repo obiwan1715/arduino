d434e56dce3c3c45f90c555e64b21437c4b086b6
obiwan1715 <benatherton.news@gmail.com>
2017-08-17 15:19:08 +1000

    servo rotates star wars lego tower

---
+++ starWarsTower/starWarsTower.ino
@@ -0,0 +1,32 @@
+//star wars lego tower sweeps through a 90 degree arc searching for rebels on the ice planet Hoth
+
+#include <Servo.h> //include the servo library
+
+Servo myservo;  // create servo object to control a servo
+
+int pos = 90;    // variable to store the servo position at roght angles to servo body
+int creep = 200; // adjust this var to change the speed of sweep
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(9600);
+}
+
+void loop() {
+  for (pos = 90; pos <= 135; pos ++) {  // goes from 90 degrees to 135 degrees in steps of 1 degree
+    myservo.write(pos);                 // tell servo to go to position in variable 'pos'
+    Serial.print(pos);                  // print pos in degrees to terminal
+    delay(creep);                       // wait before advancing
+  }
+  for (pos = 135; pos >= 45; pos --) {  // goes from 135 degrees to 45 degrees
+    myservo.write(pos);
+    Serial.print(pos);
+    delay(creep);
+  }
+  for (pos = 45; pos <= 90; pos++) {    // goes from 45 back to 90
+    myservo.write(pos);
+    Serial.print(pos);
+    delay(creep);    
+  }
+}
+
