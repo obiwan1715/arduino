c4b051ba773ff27f0077a46420de9de5e53b46a7
obiwan1715 <benatherton.news@gmail.com>
2017-08-30 16:44:44 +1000

    test prog for new better photocell

---
+++ keyestudiotest/keyestudiotest.ino
@@ -0,0 +1,22 @@
+/*
+ * basic test for keyestudio photo cell
+ * low light = low 'value'
+ * includes sound
+ * sound would be better with a map function
+ * calibrate seems to make the values very extreme - see thereminMkI
+ */
+
+int sensorPin = A0 ;
+int value = 0;
+const int soundOut = 9;
+
+void setup() {
+Serial.begin(9600);
+}
+
+void loop() {
+value = analogRead(sensorPin);
+tone(soundOut,value,20); //send tone to pin 9, play pitch, duration 20 (can change duration to change sound effect)
+Serial.println(value, DEC);
+delay(50);
+}
