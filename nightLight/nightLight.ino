ebf5ed5ee0eaabf67b4200975af1ba5f4fff5215
obiwan1715 <benatherton.news@gmail.com>
2017-08-31 20:26:01 +1000

    tested nightlight and confirmed it works

--- nightLight/nightLight.ino
+++ nightLight/nightLight.ino
@@ -1,21 +1,19 @@
 /*night light
  * uses if/else loop
- * reads input from photo cell
- * if falls below a certain level, turns on a led
- * use bits of old maglite to house the led and miinimise light falling back on the photo cell
- * ... hopefully this will stop it going into a feedback loop 
- * UNTESTED
+ * reads input from photo cell on AO
+ * if falls below a certain level, turns on a led on pin 9
+ * uses head of old maglite to house the led and miinimise light falling back on the photo cell
  */
 
 const int sensorPin = A0 ; // read input from photo cell on A0
 const int bulb = 9; // LED connect to pin 9
 int light = 0;  // set initial record value of photocell input
-int dark = 100; // this will be the cut off point -- below this counts as dark and the light comes on -- ABRITRARY NUMBER ATM
-
+int dark = 200; // this will be the cut off point -- below this counts as dark and the light comes on
 
 void setup() {
 
 // Serial.begin(9600); // use this to print to serial port for calibration if needed
+
 pinMode(sensorPin, INPUT);
 pinMode(bulb, OUTPUT);
 
