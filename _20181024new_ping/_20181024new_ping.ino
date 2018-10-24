2134bebad49896ecfb782e21fe03108a796cce90
obiwan1715 <benatherton.news@gmail.com>
2018-10-24 17:57:55 +1000

    ping works

---
+++ _20181024new_ping/_20181024new_ping.ino
@@ -0,0 +1,23 @@
+/* this works
+ *  returns cm vales depending on distance from object
+ *  could use this for basic obstacle sensing robot
+ */
+
+#include <NewPing.h>
+ 
+#define TRIGGER_PIN  12
+#define ECHO_PIN     11
+#define MAX_DISTANCE 200
+ 
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+ 
+void setup() {
+  Serial.begin(115200);
+}
+ 
+void loop() {
+  delay(50);
+  Serial.print("Ping: ");
+  Serial.print(sonar.ping_cm());
+  Serial.println("cm");
+}
