cf4a79fc45032cea4b169bb7df12b6b0cc5f2476
obiwan1715 <benatherton.news@gmail.com>
2018-10-25 15:50:28 +1000

    start work on insect robot

---
+++ insect_robot_mark_one/test_ping_with_builtin_led/test_ping_with_builtin_led.ino
@@ -0,0 +1,32 @@
+/* this works with built-in LED
+ *  triggers at MAX_DISTANCE or closer
+ */
+
+#include <NewPing.h>
+ 
+#define TRIGGER_PIN  12
+#define ECHO_PIN     11
+#define MAX_DISTANCE 15
+int distance = 0;
+ 
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+ 
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+ 
+void loop() {
+  delay(50);
+  distance = sonar.ping_cm();
+  Serial.print("Ping: ");
+  Serial.print(sonar.ping_cm());
+  Serial.println("cm");
+  if (distance > 0) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+    
+  }
---
+++ insect_robot_mark_one/test_ping_with_external_led/test_ping_with_external_led.ino
@@ -0,0 +1,33 @@
+/* this works with external LED on ledPin
+ *  triggers at MAX_DISTANCE or closer
+ */
+
+#include <NewPing.h>
+ 
+#define TRIGGER_PIN  12
+#define ECHO_PIN     11
+#define MAX_DISTANCE 15
+const int ledPin = 7;
+int distance = 0;
+ 
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+ 
+void setup() {
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+}
+ 
+void loop() {
+  delay(50);
+  distance = sonar.ping_cm();
+  Serial.print("Ping: ");
+  Serial.print(sonar.ping_cm());
+  Serial.println("cm");
+  if (distance > 0) {
+    digitalWrite(ledPin, HIGH);
+  }
+  else {
+    digitalWrite(ledPin, LOW);
+  }
+    
+  }
